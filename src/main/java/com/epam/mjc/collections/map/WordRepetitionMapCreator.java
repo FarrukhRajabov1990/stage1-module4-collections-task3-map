72f5a86e1efd04627326f5722a8b7c82e9e55db3
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-10-12 00:20:24 +0500

    Initial commit

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        Map<String, Integer> result = new HashMap<>();
+        List<String> splitWords = List.of(sentence.toLowerCase().split("[ ,;.]"));
+
+        Set<String> uniqueWords = new HashSet<>(splitWords);
+        int counter = 0;
+
+        for (String unique : uniqueWords) {
+            for (String splitWord : splitWords) {
+                if (unique.equals(splitWord)) {
+                    counter++;
+                }
+            }
+            result.put(unique, counter);
+            counter = 0;
+        }
+
+        return result;
     }
 }
