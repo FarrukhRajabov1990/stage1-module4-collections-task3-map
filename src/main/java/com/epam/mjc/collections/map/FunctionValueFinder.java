9798dd2226fb7e3535b22c8fdffb5e508eda41f7
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-10-12 09:00:57 +0500

    Second commit

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -7,12 +7,20 @@ import java.util.Map;
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
-        return true;
+        for (Integer value : functionMap.values()) {
+            if (value == requiredValue) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
 
         Map<Integer, Integer> result = new HashMap<>();
+        for (Integer key : sourceList) {
+            result.put(key, (5*key + 2));
+        }
         return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,12 +1,28 @@
 package com.epam.mjc.collections.map;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
 
         Map<String, Integer> result = new HashMap<>();
+        Set<String> uniqueSets = new HashSet<>(sourceMap.values());
+        int minNumber = Integer.MAX_VALUE;
+        for (String unique : uniqueSets) {
+            for (Integer number : sourceMap.keySet()) {
+                if (unique.equals(sourceMap.get(number))) {
+                    if (number < minNumber) {
+                        minNumber = number;
+                    }
+                }
+            }
+            result.put(unique, minNumber);
+            minNumber = Integer.MAX_VALUE;
+        }
+
         return result;
     }
 }
